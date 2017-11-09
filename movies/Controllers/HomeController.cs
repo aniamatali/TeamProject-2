cddf16907036e0a4dbf4322138d14a155bdc8e0a
alvin niamatali <aniamatali@icloud.com>
2017-11-09 09:49:53 -0800

    account creation working now final

--- movies/Controllers/HomeController.cs
+++ movies/Controllers/HomeController.cs
@@ -29,7 +29,7 @@ namespace EMDB.Controllers
           {
             newUser.Save();
           }
-          return View("index");
+          return View("index",newUser);
         }
 
         [HttpPost("/homepage")]
